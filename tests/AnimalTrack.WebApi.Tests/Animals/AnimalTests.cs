4a17c696d6869f0ec963fca266a471d136c42538
Mitchell Wright <mitchell.wright877@gmail.com>
2025-03-27 22:31:39 +0000

    Added more test coverage for notes

--- tests/AnimalTrack.WebApi.Tests/Animals/AnimalTests.cs
+++ tests/AnimalTrack.WebApi.Tests/Animals/AnimalTests.cs
@@ -6,7 +6,7 @@ using AnimalTrack.WebApi.Tests.Fixtures;
 using Shouldly;
 using Xunit;
 
-namespace AnimalTrack.WebApi.Tests;
+namespace AnimalTrack.WebApi.Tests.Animals;
 
 public class AnimalTests(AnimalTrackFixture animalTrackFixture) : IClassFixture<AnimalTrackFixture>
 {
@@ -96,6 +96,48 @@ public class AnimalTests(AnimalTrackFixture animalTrackFixture) : IClassFixture<
     }
 
     [Fact]
+    public async Task GivenKnownAnimal_WhenCreateNote_ShouldReturn200()
+    {
+        // Arrange
+        var uri = new Uri("Animal/1/notes", UriKind.Relative);
+        
+        // Act
+        var response = await _httpClient.PostAsync(uri, JsonContent.Create("This is a cool note"));
+        
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GivenUnknownAnimal_WhenCreateNote_ShouldReturn404()
+    {
+        // Arrange
+        var uri = new Uri("Animal/99/notes", UriKind.Relative);
+        
+        // Act
+        var response = await _httpClient.PostAsync(uri, JsonContent.Create("This is a cool note"));
+        
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("    ")]
+    public async Task GivenKnownAnimal_WhenCreateEmptyNote_ShouldReturn400(string? note)
+    {
+        // Arrange
+        var uri = new Uri("Animal/1/notes", UriKind.Relative);
+        
+        // Act
+        var response = await _httpClient.PostAsync(uri, JsonContent.Create(note));
+        
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
     public async Task GivenKnownAnimal_WhenGetNotes_ShouldReturn200()
     {
         // Arrange
@@ -111,5 +153,44 @@ public class AnimalTests(AnimalTrackFixture animalTrackFixture) : IClassFixture<
         notes.Count.ShouldBe(2);
         notes.ShouldContain(note => note.Note == "This is a note");
         notes.ShouldContain(note => note.Note == "This is my second note");
+    }
+
+    [Fact]
+    public async Task GivenUnknownAnimal_WhenGetNotes_ShouldReturn404()
+    {
+        // Arrange
+        var uri = new Uri("Animal/99/notes", UriKind.Relative);
+        
+        // Act
+        var response = await _httpClient.GetAsync(uri);
+        
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task GivenKnownNote_WhenDelete_ShouldReturn200()
+    {
+        // Arrange
+        var uri = new Uri("Animal/note/99", UriKind.Relative);
+        
+        // Act
+        var response = await _httpClient.DeleteAsync(uri);
+        
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GivenUnknownNote_WhenDelete_ShouldReturn404()
+    {
+        // Arrange
+        var uri = new Uri("Animal/note/99", UriKind.Relative);
+        
+        // Act
+        var response = await _httpClient.DeleteAsync(uri);
+        
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
     }
 }
