d46fed477d7bac1d5298911e624ea627d3458c1e
Mitchell Wright <mitchell.wright877@gmail.com>
2025-03-31 10:51:24 +0100

    Rejigged workflow to compare

---
+++ .github/scripts/compareBenchmarks.js
@@ -0,0 +1,58 @@
+const fs = require("node:fs");
+
+module.exports = ({github, context}) => {
+    const masterResults = loadBenchmarkResults('master-benchmark-results.json');
+    const branchResults = loadBenchmarkResults('branch-benchmark-results.json');
+    
+    let commentContent = '| Benchmark | Timings | Allocations |'
+    commentContent += '|---|---|---|'
+    
+    for(const branchResult of branchResults) {
+        const masterResult = masterResults.filter(res => res.name == branchResult.name);
+        
+        const name = branchResult.name;
+        const timingsDiff = percentageDifference(branchResult.mean, masterResult.mean);
+        const allocationsDiff = percentageDifference(branchResult.allocated, masterResult.allocated);
+        
+        commentContent += `| ${name} | ${round(timingsDiff, 2)}% | ${round(allocationsDiff, 2)} |`
+    }
+    
+    github.rest.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: commentContent
+    });
+}
+
+function loadBenchmarkResults(filePath) {
+    const fileContent = fs.readFileSync(filePath);
+    const parsedFileContent = JSON.parse(fileContent);
+    const benchmarks = parsedFileContent['Benchmarks']
+
+    const results = [];
+    for (const benchmark of benchmarks) {
+        const stats = benchmark['Statistics'];
+        const memory = benchmark['Memory'];
+        const result = {
+            name: benchmark['Type'],
+            mean: stats['Mean'],
+            gen0: memory['Gen0Collections'],
+            gen1: memory['Gen1Collections'],
+            gen2: memory['Gen2Collections'],
+            allocated: memory['BytesAllocatedPerOperation'],
+        }
+        results.push(result);
+    }
+
+    return results;
+}
+
+function percentageDifference(newValue, oldValue) {
+    return ((newValue - oldValue) / oldValue) * 100;
+}
+
+function round(value, decimals) {
+    var k = 10 * decimals;
+    return Math.round(value * k) / k;
+}
