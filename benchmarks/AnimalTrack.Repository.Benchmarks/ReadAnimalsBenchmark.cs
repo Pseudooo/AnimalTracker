9773e984822aeeffde3b7e7ee387f36d854bbd4f
Mitchell Wright <36946520+Pseudooo@users.noreply.github.com>
2025-04-01 08:55:28 +0100

    Support multiple benchmarks (#19)
    * Added benchmark runner project
    
    * Added benchmark for animal writing
    
    * Updated workflow
    
    * Sorted sln issue
    
    * Removed stuff
    
    * Added back
    
    * Accounting for path changes in workflow
    
    * Removed benchmarks again (im in git hell)
    
    * Added back

---
+++ benchmarks/AnimalTrack.Repository.Benchmarks/ReadAnimalsBenchmark.cs
@@ -0,0 +1,44 @@
+using AnimalTrack.Repository.Entities;
+using AnimalTrack.Repository.Interfaces;
+using AnimalTrack.Repository.Providers;
+using AnimalTrack.Repository.Repositories;
+using AnimalTrack.WebApi.Fixtures;
+using AnimalTrack.WebApi.Tests.Fixtures;
+using BenchmarkDotNet.Attributes;
+
+namespace AnimalTrack.Repository.Benchmarks;
+
+[JsonExporter]
+[MemoryDiagnoser]
+public class ReadAnimalsBenchmark
+{
+    private DatabaseFixture _databaseFixture = null!;
+    private IAnimalRepository _animalRepository = null!;
+    
+    [GlobalSetup]
+    public async Task SetUp()
+    {
+        _databaseFixture = new DatabaseFixtureBuilder()
+            .WithSeedScript("seed_1k_animals.sql")
+            .Build();
+        await _databaseFixture.StartAsync();
+        
+        var databaseConfiguration = _databaseFixture.GetDatabaseConfiguration();
+        var postgreSqlConnectionFactory = new PostgreSqlConnectionFactory(databaseConfiguration);
+        var postgreSqlClient = new PostgreSqlClient(postgreSqlConnectionFactory);
+        var postgreSqlQueryProvider = new PostgreSqlQueryProvider();
+        _animalRepository = new AnimalRepository(postgreSqlQueryProvider, postgreSqlClient);
+    }
+
+    [GlobalCleanup]
+    public async Task Cleanup()
+    {
+        await _databaseFixture.DisposeAsync();
+    }
+
+    [Benchmark]
+    public async Task<List<AnimalEntity>> GetAnimals()
+    {
+        return await _animalRepository.GetAnimalPage(1, 1000);
+    }
+}
