08464c669cdd3267b9801c5dbe3def65ec183f18
Mitchell Wright <mitchell.wright877@gmail.com>
2025-04-03 13:45:28 +0100

    Added migrations for table creation

---
+++ src/AnimalTrack.Migrations/Migrations/00_Create_Animal_Tables/02_Create_Animal_Tasks_Table.sql
@@ -0,0 +1,11 @@
+create table if not exists AnimalTasks
+(
+    Id integer generated always as identity
+        constraint AnimalTasks_pk
+            primary key,
+    AnimalId integer not null
+        references Animals (Id),
+    Name text not null,
+    CreatedAt timestamp with time zone not null
+        default now()
+)
