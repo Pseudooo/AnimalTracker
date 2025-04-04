1f64f08dcc4dbd4511581392d6871b439d271fdc
Mitchell Wright <mitchell.wright877@gmail.com>
2025-04-04 20:26:27 +0100

    Removed unused query provider methods

--- src/AnimalTrack.Repository/Interfaces/IPostgreSqlQueryProvider.cs
+++ src/AnimalTrack.Repository/Interfaces/IPostgreSqlQueryProvider.cs
@@ -3,13 +3,7 @@ namespace AnimalTrack.Repository.Interfaces;
 public interface IPostgreSqlQueryProvider
 {
     public Task<string> GetInsertAnimalSqlText();
-    Task<string> GetAnimalByIdSqlText();
-    Task<string> GetAnimalPageSqlText();
-    Task<string> GetUpdateAnimalSqlText();
     Task<string> GetInsertAnimalNoteSqlText();
-    Task<string> GetAnimalNotesSqlText();
     Task<string> DeleteAnimalNoteSqlText();
-    Task<string> GetAnimalTasksSqlText();
     Task<string> GetInsertAnimalTaskSqlText();
-    Task<string> GetUpdateAnimalTaskSqlText();
 }
