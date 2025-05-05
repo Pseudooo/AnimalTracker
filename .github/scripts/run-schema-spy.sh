6e219aeeedaf72721b2661aa45184b96c7ecd3c1
Mitchell Wright <mitchell.wright877@gmail.com>
2025-05-05 11:53:52 +0100

    Using correct flag for jdbc driver

--- .github/scripts/run-schema-spy.sh
+++ .github/scripts/run-schema-spy.sh
@@ -5,4 +5,4 @@ echo "Downloading Postgres JDBC driver"
 curl -L "https://jdbc.postgresql.org/download/postgresql-42.5.4.jar" --output ./postgres-jdbc-driver.jar
 
 echo "Generating Diagram"
-java -jar ./schemaspy.jar -t pgsql11 -db ./postgres-jdbc-driver.jar -db AnimalTrack -host localhost -post 5432 -u postgres -p WhatTheFooBar123 -o ./schema-export
+java -jar ./schemaspy.jar -t pgsql11 -dp ./postgres-jdbc-driver.jar -db AnimalTrack -host localhost -post 5432 -u postgres -p WhatTheFooBar123 -o ./schema-export
